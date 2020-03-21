7aa08571da842377bdca192726fa68a68acd257b
Andrus Adamchik <andrus@objectstyle.com>
2020-03-21 11:15:00 +0300

    refactoring, cleanup

--- src/main/java/io/bootique/swagger/demo/Application.java
+++ src/main/java/io/bootique/swagger/demo/Application.java
@@ -8,26 +8,29 @@ import io.swagger.jaxrs.config.BeanConfig;
 
 public class Application implements Module {
 
-    public Application() {
-        BeanConfig conf = new BeanConfig();
-        conf.setTitle("Bootique Swagger Demo");
-        conf.setDescription("Demonstrates Swagger v2 API spec generation and presentation");
-        conf.setVersion("1.0");
-        conf.setHost("localhost:8080");
-        conf.setBasePath("/");
-        conf.setResourcePackage("io.bootique.swagger.demo");
-        conf.setSchemes(new String[] { "http" });
-
-        // quite counterintuitively, the following setter initializes Swagger metadata for the entire app.
-        conf.setScan(true);
-    }
-
     public static void main(String[] args) {
+
+        initApiMetadata();
+
         Bootique.app(args)
                 .autoLoadModules()
                 .module(Application.class)
                 .exec()
                 .exit();
+    }
+
+    private static void initApiMetadata() {
+        BeanConfig conf = new BeanConfig();
+        conf.setTitle("Bootique Swagger Demo");
+        conf.setDescription("Demonstrates Swagger v2 API spec generation and presentation");
+        conf.setVersion("1.1");
+        conf.setHost("localhost:8080");
+        conf.setBasePath("/");
+        conf.setResourcePackage("io.bootique.swagger.demo");
+        conf.setSchemes(new String[]{"http"});
+
+        // quite counterintuitively, the following setter initializes Swagger metadata for the entire app.
+        conf.setScan(true);
     }
 
     @Override
