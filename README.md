4ea9b2d7b14ca02dfb7462ae6858d40d7e2181b7
Andrus Adamchik <andrus@objectstyle.com>
2020-03-21 10:51:26 +0300

    simplifying the example

--- README.md
+++ README.md
@@ -16,22 +16,22 @@ mvn package
 Enter the following to launch application with default url to swagger.json:
 
 ```bash
-java -jar target/bootique-swagger-demo-1.1-SNAPSHOT.jar
+java -jar target/bootique-swagger-demo-1.1-SNAPSHOT.jar --server
 ```
 
-Enter the following to launch app with custom url to swagger.json configured in yml file:
+Enter the following to launch app with custom url to `swagger.json` configured in yml file:
 
 ```bash
-java -jar target/bootique-swagger-demo-1.1-SNAPSHOT.jar --config=classpath:customUrl.yml
+java -jar target/bootique-swagger-demo-1.1-SNAPSHOT.jar --server --config=customUrl.yml
 ```
 
 After launching the app, go to
 
-1. http://localhost:9999/api Get hello page
-2. http://localhost:9999/swagger.json Documented by swagger application api as JSON
-3. http://localhost:9999/swagger.yaml Documented by swagger application api as YAML
-3. http://localhost:9999/swagger Swagger Web console
-4. http://localhost:9999/static Static resources
+1. http://localhost:8080/api Get hello page
+2. http://localhost:8080/swagger.json Documented by swagger application api as JSON
+3. http://localhost:8080/swagger.yaml Documented by swagger application api as YAML
+3. http://localhost:8080/swagger Swagger Web console
+4. http://localhost:8080/static Static resources
 
 
 
--- src/main/java/io/bootique/swagger/demo/Application.java
+++ src/main/java/io/bootique/swagger/demo/Application.java
@@ -2,23 +2,23 @@ package io.bootique.swagger.demo;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
-import io.bootique.BQCoreModule;
 import io.bootique.Bootique;
 import io.bootique.jersey.JerseyModule;
-import io.bootique.jetty.command.ServerCommand;
 import io.swagger.jaxrs.config.BeanConfig;
 
 public class Application implements Module {
 
     public Application() {
         BeanConfig conf = new BeanConfig();
-        conf.setTitle("Bootique swagger");
-        conf.setDescription("Swagger demo api");
+        conf.setTitle("Bootique Swagger Demo");
+        conf.setDescription("Demonstrates Swagger v2 API spec generation and presentation");
         conf.setVersion("1.0");
-        conf.setHost("localhost:9999");
+        conf.setHost("localhost:8080");
         conf.setBasePath("/");
         conf.setResourcePackage("io.bootique.swagger.demo");
         conf.setSchemes(new String[] { "http" });
+
+        // quite counterintuitively, the following setter initializes Swagger metadata for the entire app.
         conf.setScan(true);
     }
 
@@ -32,11 +32,6 @@ public class Application implements Module {
 
     @Override
     public void configure(Binder binder) {
-        BQCoreModule.extend(binder)
-                .setDefaultCommand(ServerCommand.class)
-                .addConfig("classpath:bootique.yml");
-
-        JerseyModule.extend(binder)
-                .addResource(HelloApi.class);
+        JerseyModule.extend(binder).addResource(HelloApi.class);
     }
 }
